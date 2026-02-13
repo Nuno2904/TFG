0def94caed6ea5dbda322b10d12a0595fa10507d
Nuno Escudero <nuescudero@usal.es>
2026-02-13 01:11:37 +0100

    models.py
    creado el .py models y conectado la base de datos a pgmyadmin. Corrección de fallo en la url de la base de datos

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+from . database import engine
+from . import models
 
+
+models.Base.metadata.create_all(bind = engine) #crea las tablas en la base de datos, si no existen, a partir de los modelos definidos en models.py.
 app = FastAPI()
 @app.get("/")
 def read_root():
